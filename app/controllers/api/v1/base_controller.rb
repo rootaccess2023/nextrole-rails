ab4b983275fba8f9e46f79d574be0a561ba0bb2f
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2026-03-26 20:37:26 +0100

    fix: skip CSRF for API controllers

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::BaseController < ActionController::API
+  skip_before_action :verify_authenticity_token
+  
   def authenticate_user!
     token   = extract_token_from_header
     payload = JsonWebToken.decode(token)
